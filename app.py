5c53da8293952e863c113fd832c61a0eb58d9aef
Thibault Clerice <leponteineptique@gmail.com>
2016-04-11 15:59:01 +0200

    Working docker image

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request
 from flask.ext.nemo import Nemo
-from nautilus.flask_ext import FlaskNautilus
+from capitains_nautilus.flask_ext import FlaskNautilus
 from werkzeug.contrib.cache import FileSystemCache
 from flask_cache import Cache
 
@@ -20,13 +20,15 @@ nautilus = FlaskNautilus(
 nemo = Nemo(
     app=app,
     name="nemo",
-	base_url="/",
-	api_url="http://127.0.0.1:5000/api/cts"
+	base_url="",
+	api_url="/api/cts",
+    endpoint=nautilus.endpoint
 )
 # We register its routes
 nemo.register_routes()
 # We register its filters
 nemo.register_filters()
 
+app.debug = True
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
