8c10438d14f937c58c20d1a2b984349a5adb8db4
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2017-02-08 19:58:56 -0500

    Fix single quotes in build script

--- rebuild-docker.sh
+++ rebuild-docker.sh
@@ -9,11 +9,11 @@ if [[ -d nemo_plokamos_plugin || -L nemo_plokamos_plugin ]]; then
 		-v $NEMO_DATA:/opt/data \
 		-v $NEMO_CACHE:/opt/cache \
 		-v $NEMO_ASSETS:/code/nemo_plokamos_plugin/nemo_plokamos_plugin/data/assets \
-		-e PERSEIDS_API_KEY=\'$PERSEIDS_API_KEY\' \
-		-e PERSEIDS_API_SECRET=\'$PERSEIDS_API_SECRET\' \
-		-e APP_SECRET_KEY=\'$APP_SECRET_KEY\' \
-		 -e SPARQL_UPDATE_ENDPOINT=\'$SPARQL_UPDATE_ENDPOINT\' \
-		 -e SPARQL_SELECT_ENDPOINT=\'$SPARQL_SELECT_ENDPOINT\' \
+		-e PERSEIDS_API_KEY=$PERSEIDS_API_KEY \
+		-e PERSEIDS_API_SECRET=$PERSEIDS_API_SECRET \
+		-e APP_SECRET_KEY=$APP_SECRET_KEY \
+		 -e SPARQL_UPDATE_ENDPOINT=$SPARQL_UPDATE_ENDPOINT \
+		 -e SPARQL_SELECT_ENDPOINT=$SPARQL_SELECT_ENDPOINT \
 		nemo:latest
 	else
 		echo "Plokamos API keys missing [plokamos_keys.sh]."
